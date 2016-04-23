7ae63918cce2e9120012902c277e6439f5ac55ac
Verena Link <vlink@ucsd.edu>
2016-04-22 17:00:42 -0700

    minor bug fixes

--- analysis/add_snps_textfile.pl
+++ analysis/add_snps_textfile.pl
@@ -2,36 +2,40 @@
 
 use strict;
 use Getopt::Long;
-use DBI;
-require '../general/config.pm';
-require '../general/system_interaction.pm';
+BEGIN {push @INC, '/home/vlink/mouse_strains/marge/general'}
+use config;
+BEGIN {push @INC, '/home/vlink/mouse_strains/marge/db_part'};
+use processing;
 use Data::Dumper;
+my $config = config::read_config();
 	
 $|=1;
 
-if(@ARGV < 1) {
+sub printCMD{
         print STDERR "Usage:\n";
         print STDERR "General commands:\n";
         print STDERR "\t-genome <genome>\n";
         print STDERR "\t-file <input file>: coordinates have to be the reference coordinates\n";
 	print STDERR "\t-output <output name>: default <file name>_snps.txt\n";
-        print STDERR "\t-strains: one or several strains (comma separated) - all or not specified: all strains\n";
+        print STDERR "\t-strains: one or several strains (comma separated)\n";
 	print STDERR "\n\nOptions for RNA-Seq:\n";
+	print STDERR "\tAnnotation RNA-Seq esp. with exons takes a while\n";
 	print STDERR "\t-exons: Just looks for mutations within the exons\n";
-	print STDERR "\t-all: Looks for mutations in exons and introns\n";
-	print STDERR "Annotation RNA-Seq esp. with exons takes a while\n";
-        print STDERR "\n\nFormat of input file:\n";
-        print STDERR "\t-homer: shifts HOMER peak and RNA files (default)\n";
-        print STDERR "\t-format: checks formats.txt to find the user defined format called annotation_format\n";
-	print STDERR "\t-homo: Strains are homozygous\n";
+	print STDERR "\t-refseq_file: File with RefSeq IDs (check HOMER format)\n";
+	print STDERR "\t-gene_file: File with Gene IDs (check HOMER format)\n";
+	print STDERR "\t-hetero: Strains are heterozygous\n";
         exit;
 }
 
+if(@ARGV < 1) {
+	&printCMD();
+}
 
-$_ = "" for my($genome, $file, $output, $split_sign, $chr_pos, $shift_order, $start, $end, $tmp_start, $transcript);
-$_ = 0 for my($homer, $format, $chr, $print_header, $exons, $aall, $found, $total, $count, $homo);
-$_ = () for my(@strains, @split, $dbh, $sth, $sth_exon, $header, %lookup_header, $mutations);
-my $aallele = 2;
+
+$_ = "" for my($genome, $file, $output, $split_sign, $chr_pos, $shift_order, $start, $end, $tmp_start, $transcript, $lin, $line, $filename, $gene_file, $refseq_file, $ref, $exon_ref, $strand, $header, $chr_tmp);
+$_ = 0 for my($homer, $format, $chr, $print_header, $exons, $all, $found, $total, $count, $hetero);
+$_ = () for my(@strains, @split, $dbh, $sth, $sth_exon,  %lookup_header, $mutations, %peaks, %exons, @exon_split, %all_exons);
+my $allele = 2;
 
 my %mandatory = ('-genome' => 1, '-file' => 1);
 my %convert = map { $_ => 1 } @ARGV;
@@ -41,89 +45,120 @@ my $data = config::read_config()->{'data_folder'};
 
 GetOptions(     "genome=s" => \$genome,
                 "file=s" => \$file,
+		"gene_file=s" => \$gene_file,
+		"refseq_file=s" => \$refseq_file,
 		"output=s" => \$output,
                 "strains=s{,}" => \@strains,
 		"exons" => \$exons,
-		"all" => \$aall,
-                "homer" => \$homer,
-		"homo" => \$homo,
+		"all" => \$all,
+		"hetero" => \$hetero,
                 "format" => \$format)
         or die ("Error in command line arguments!\n");
 
 
-my $aa = `wc -l $file`;
-$total = ((split('\s+', $aa))[0]);
 
-if($homo == 1) {
-	$aallele = 1;
+if($hetero == 1) {
+	$allele = 2;
+} else {
+	$allele = 1;
 }
 
-my $ref_as_strain = 0;
 for(my $i = 0; $i < @strains; $i++) {
         chomp $strains[$i];
         $strains[$i] =~ s/,//g;
-        if($strains[$i] eq "reference") {
-                $ref_as_strain = 1;
-        }
+	$strains[$i] = uc($strains[$i]);
 }
-
-if(@strains == 0 || $strains[0] eq "all") {
-        my $hash = config::get_strains($genome);
-        my $i = 0;
-        foreach my $key (keys %{$hash}) {
-                $strains[$i] = $key;
-                $i++;
-                if($key eq "reference") {
-                        $ref_as_strain = 1;
-                }
-        }
-}
-
-if($ref_as_strain == 0) {
-        push(@strains, "reference");
-}
-
-if($homer == 0 && $format == 0) {
-	$homer = 1;
-}
-
-#Get format
-open FH, "<../config/formats.txt";
-
-#Read format
-foreach my $line (<FH>) {
-        chomp $line;
-	if($line eq "") { next; }
-        @split = split(':', $line);
-        if($homer == 1 && $split[0] eq "annotation_homer") {
-                ($split_sign, $chr_pos, $shift_order) = config::read_format($split[1]);
-        } elsif($homer == 1 && $split[0] eq "header_homer") {
-		($header) = config::read_header($split[1]);
-	} elsif($format == 1 && $split[0] eq "annotation_format") {
-                ($split_sign, $chr_pos, $shift_order) = config::read_format($split[1]);
-        } elsif($format == 1 && $split[0] eq "header_format") {
-		($header) = config::read_header($split[1]);
-	}
-}
-
-for(my $i = 0; $i < @{$header}; $i++) {
-	if($header->[$i] eq "") {
-		print STDERR "Header is not defined for this file!\n";
-	}
-	$lookup_header{$header->[$i]} = 1;
-}
-
-#Read in file
 
 if($output eq "") {
 	#Check if input filename has a file ending
 	@split = split('\.', $file);
 	$output = $split[0] . "_snps.txt";
 }
+
+open FH, "<$file";
+foreach my $line (<FH>) {
+	chomp $line;
+	@split = split('\t', $line);
+	if(substr($split[1], 0, 3) ne "chr" || length($split[1]) < 4 ) { $header = $line; next; }
+	$peaks{substr($split[1], 3)}{$split[2]}{'end'} = $split[3];
+	$peaks{substr($split[1], 3)}{$split[2]}{'id'} = $split[0];
+	$peaks{substr($split[1], 3)}{$split[2]}{'line'} = $line;
+}
+close FH;
+if($exons == 1) {
+	($refseq_file, $gene_file) = processing::check_homer_files($refseq_file, $gene_file, $config, 0, $genome);
+	$exon_ref = processing::save_transcript($refseq_file);
+	%all_exons = %{$exon_ref};
+}
+#Start to collect mutations
+print STDERR "Processing:\n";
+foreach my $chr (sort {$a cmp $b } keys %peaks) {
+	print STDERR "\t\tchromosome " . $chr . " (" . (keys %{$peaks{$chr}}) . " peaks/transcripts) \n";
+	for(my $i = 0; $i < @strains; $i++) {
+		for(my $al = 1; $al <= $allele; $al++) {
+			$filename = $data ."/" . uc($strains[$i]) . "/chr" . $chr . "_allele_" . $al . ".mut";
+			if(!-e $filename) {
+				print STDERR "\t\tCan't open $filename\n";
+				foreach my $start (keys %{$peaks{$chr}}) {
+					$peaks{$chr}{$start}{$strains[$i]} = "";
+				}
+				next;
+			}
+	                open my $fh_mut, "<", "$filename" or die "Can't open $filename\n";
+			$line = read_file_line($fh_mut);
+			@split = split('\t', $line);
+			foreach my $start (sort {$a <=> $b} keys %{$peaks{$chr}}) {
+				$peaks{$chr}{$start}{$strains[$i]} = "";
+				%exons = ();
+				if($exons == 1) {
+					%exons = %{$all_exons{$peaks{$chr}{$start}{'id'}}};
+				} else {
+					$exons{$start . "_" . $peaks{$chr}{$start}{'end'}} = 0;
+				}
+				foreach my $e (sort {$exons{$a} <=> $exons{$b} } keys %exons) {
+					@exon_split = split("_", $e);
+					while($line and $split[0] < $exon_split[0]) {
+						$line = read_file_line($fh_mut);
+						if($line) {
+							@split = split('\t', $line);
+						}
+					}
+					while($line and $split[0] < $exon_split[1]) {
+						$peaks{$chr}{$start}{$strains[$i]} .= $split[0] . "(" . $al . "):" . $split[1] . "->" . $split[2] . ",";
+						$line = read_file_line($fh_mut);
+						if($line) {
+							@split = split('\t', $line); 
+						}
+					}
+				}
+			}
+			close $fh_mut;
+		}
+	}
+}
+
 open OUT, ">$output";
+print OUT $header;
+for(my $i = 0; $i < @strains; $i++) {
+	print OUT "\t" . $strains[$i];
+}
+print OUT "\n";
+foreach my $chr (keys %peaks) {
+	foreach my $start (keys %{$peaks{$chr}}) {
+		print OUT $peaks{$chr}{$start}{'line'};
+		for(my $i = 0; $i < @strains; $i++) {
+			if(length($peaks{$chr}{$start}{$strains[$i]}) > 1) {
+				chop $peaks{$chr}{$start}{$strains[$i]};
+			}
+			print OUT "\t" . $peaks{$chr}{$start}{$strains[$i]};
+		}
+		print OUT "\n";
+	}
+}
+close OUT;
+exit;
 
 my @fileHandles;
-my $filename;
 my $outfile_open = 0;
 
 sub open_filehandles{
@@ -199,10 +234,8 @@ for(my $chr = 1; $chr < 19; $chr++) {
 sub read_file_line {
         my $fh = shift;
         my @split;
-
         if ($fh and my $line = <$fh>) {
                 chomp $line;
-#               return [ split('\t', $line) ];
                 return $line;
         }
         return;
@@ -213,7 +246,7 @@ sub add_mutations{
 	my $end = $_[1];
 	$mutations = "";
 	for(my $i = 0; $i < @strains; $i++) {
-		for(my $aa = 0; $aa < $aallele; $aa++) {
+		for(my $aa = 0; $aa < $allele; $aa++) {
 			$mutations .= "\t";
 			if($strains[$i] eq "reference") { next; }
 			$sth = $dbh->prepare("SELECT * FROM " . $genome . "_mutations_" . $strains[$i] . "_allele_" . ($aa + 1) . " WHERE chr = \'" . $chr . "\' AND pos >= " . $start . " AND pos <= " . $end);
