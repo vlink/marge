c7e0247654f35b5fc5affe26914aa46bbab3dbe2
Verena Link <vlink@ucsd.edu>
2016-04-22 17:00:19 -0700

    report stats for all strain comparisons

--- analysis/count_mutations_per_strain.pl
+++ analysis/count_mutations_per_strain.pl
@@ -7,13 +7,13 @@ use Getopt::Long;
 BEGIN {push @INC, '/home/vlink/mouse_strains/marge/general'};
 use config;
 #use analysis;
-$_ = () for my(%peaks, @split, @strains);
-$_ = 0 for my($pairwise);
-
+$_ = () for my(%peaks, @split, @strains, %save, @name, @filename);
+$_ = 0 for my($snps, $indels);
+$_ = "" for my ($data, $path);
 sub printCMD {
         print STDERR "Usage:\n";
         print STDERR "\t-strains <strains>: Comma separated list to count mutations\n";
-	print STDERR "\t-pairwise: Calculates all pairwise comparisons\n";
+	print STDERR "\t-data: data folder: default specified in config file\n";
         exit;
 }
 
@@ -24,101 +24,91 @@ if(@ARGV < 1) {
 my $param = config::read_config();
 
 GetOptions(	"strains=s{,}" => \@strains, 
-		"pairwise" => \$pairwise)
+		"data=s" => \$data)
         or die("Error in command line options!\n");
 
-my $data = config::read_config()->{'data_folder'};
+if($data eq "") {
+	$data = config::read_config()->{'data_folder'};
+}
 
-my $path;
-my $snps = 0;
-my $indels = 0;
-my %save = ();
-my @name;
 
-if($pairwise == 0) {
-	print "Strain\t#SNPs\t#InDels\n";
-} else {
-	print "Strain comparison\t#SNPs\t#InDels\n";
+for(my $i = 0; $i < @strains; $i++) {
+	$strains[$i] =~ s/,//g;
+	$strains[$i] = uc($strains[$i]);
 }
 
 foreach my $strains (@strains) {
-	$strains =~ s/,//g;
+	if(!-e $data . "/" . uc($strains)) {
+		next;
+	}
 	$path = $data . "/" . uc($strains) . "/*mut";
-	my @files = `ls $path`;
+	my @files = `ls $path 2> /dev/null`;
 	$snps = 0;
 	$indels = 0;
-	if($pairwise == 0) {
-		print $strains . "\t";
-	} else {
-		print STDERR "Reading in mutations for " . $strains . "\n";
-	}
+	print STDERR "Reading in mutations for " . $strains . "\n";
 	foreach my $f (@files) {
+		if(-e $f) {
+			print STDERR "Could not find $f\n";
+			exit;
+		}
 		chomp $f;
-		open FH, "<$f";
+		open FH, "<$f" or die "Can't open $f: $!";
+		@filename = split('/', $f);
+		@name = split("_", $filename[-1]);
+		if(@name == 1) {
+			@name = split('\.', $filename[-1]);
+			$save{$strains}{$name[0]}{0} = 1;
+			next;
+		}
 		foreach my $line (<FH>) {
 			chomp $line;
 			@split = split('\t', $line);
-			if($pairwise == 0) {
-				if(length($split[1]) == length($split[2])) {
-					$snps++;
-				} else {
-					$indels++;
-				}
-			} else {
-				@name = split("_", $f);
-				$save{$strains}{$name[0]}{$split[0]} = $split[2];			
-			}
+			$save{$strains}{$name[0]}{$split[0]} = $split[2];
 		}
-	}
-	if($pairwise == 0) {
-		print $snps . "\t" . $indels . "\n";
 	}
 }
 
-my %seen;
-
-if($pairwise == 1) {
-	for(my $i = 0; $i < @strains - 1; $i++) {
-		for(my $j = $i + 1; $j < @strains; $j++) {
-			$snps = 0;
-			$indels = 0;
+print "Strain comparison\t#SNPs\t#InDels\n";
+for(my $i = 0; $i < @strains - 1; $i++) {
+	for(my $j = $i +1 ; $j < @strains; $j++) {
+		$snps = 0;
+		$indels = 0;
+		if(exists $save{$strains[$i]}) {
 			foreach my $chr (keys %{$save{$strains[$i]}}) {
-				%seen = ();
 				foreach my $pos (keys %{$save{$strains[$i]}{$chr}}) {
-					if(exists $save{$strains[$j]}{$chr}{$pos}) {
-				#		print $save{$strains[$i]}{$chr}{$pos} . "\t" . $save{$strains[$j]}{$chr}{$pos} . "\n";
-						if($save{$strains[$i]}{$chr}{$pos} ne $save{$strains[$j]}{$chr}{$pos}) {
-							if(length($save{$strains[$i]}{$chr}{$pos}) != length($save{$strains[$j]}{$chr}{$pos}) && (length($save{$strains[$i]}{$chr}{$pos}) > 1 || length($save{$strains[$j]}{$chr}{$pos}) > 1)) {
-								$indels++;
-							} else {
-								$snps++;
-							}
-						}
-					} else {
-					#	print $save{$strains[$i]}{$chr}{$pos} . "\tNA\n";
+					if(!exists $save{$strains[$j]}{$chr}{$pos}) {
 						if(length($save{$strains[$i]}{$chr}{$pos}) > 1) {
 							$indels++;
 						} else {
 							$snps++;
 						}
-					}	
-					$seen{$pos} = 1;
-				#	print $snps . "\t" . $indels . "\n";
-				}
-				foreach my $pos (keys %{$save{$strains[$j]}{$chr}}) {
-					if(exists $seen{$pos}) {
-						next;
-					}
-				#	print "NA\t" . $save{$strains[$j]}{$chr}{$pos} . "\n";
-					if(length($save{$strains[$j]}{$chr}{$pos}) > 1) {
-						$indels++;
 					} else {
-						$snps++;
+						if($save{$strains[$j]}{$chr}{$pos} ne $save{$strains[$i]}{$chr}{$pos}) {
+							if(length($save{$strains[$i]}{$chr}{$pos}) == length($save{$strains[$j]}{$chr}{$pos})) {
+								$snps++;
+							} else {
+								$indels++;
+							}
+						}
 					}
-				#	print $snps . "\t" . $indels . "\n";
 				}
 			}
-			print $strains[$i] . " vs " . $strains[$j] . "\t" . $snps . "\t" . $indels . "\n";
 		}
+		if(exists $save{$strains[$j]}) {
+			foreach my $chr (keys %{$save{$strains[$j]}}) {
+				foreach my $pos (keys %{$save{$strains[$j]}{$chr}}) {
+					if(!exists $save{$strains[$i]}{$chr}{$pos}) {
+						if(length($save{$strains[$j]}{$chr}{$pos}) > 1) {
+							$indels++;
+						} else {
+							$snps++;
+						}
+					}
+				}
+			}
+		}
+		$snps =~ s/(\d)(?=(\d{3})+(\D|$))/$1\,/g;
+		$indels =~ s/(\d)(?=(\d{3})+(\D|$))/$1\,/g;
+		print "" . $strains[$i] . " vs " . $strains[$j] . "\t" . $snps . "\t" . $indels . "\n";
 	}
 }
