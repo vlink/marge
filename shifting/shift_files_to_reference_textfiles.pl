c816e387ff2f6a5d23cb1da1478220aa85e93b3b
Verena Link <vlink@ucsd.edu>
2017-07-05 16:15:59 -0700

    Change how to read/write to files

--- shifting/shift_files_to_reference_textfiles.pl
+++ shifting/shift_files_to_reference_textfiles.pl
@@ -1,6 +1,7 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 BEGIN {push @INC, '/home/vlink/mouse_strains/marge/general'}
 use strict;
+use warnings;
 use Getopt::Long;
 use Storable;
 use config;
@@ -8,7 +9,7 @@ use general;
 use Set::IntervalTree;
 use Data::Dumper;
 
-$_ = 0 for my ($sam, $peak, $tag, $help, $print_header, $allele, $bed);
+$_ = 0 for my ($sam, $peak, $tag, $help, $print_header, $allele, $bed, $bismark);
 $_ = "" for my ($dir, $chr, $last, $data_dir, $out_name, $last_strain);
 $_ = () for my (@files, @strains, @split, %last, %lookup, %tree, @tmp, $fetch, $pos_shifted);
 
@@ -26,6 +27,7 @@ sub printCMD {
 	print STDERR "\t-homer: shifts HOMER peak and RNA files\n";
 	print STDERR "\t-tag: shifts tag directory\n";
 	print STDERR "\t-bed: shifts bed file\n";
+	print STDERR "\t-bismark: shifts cytosine report output form BISMARK\n";
 	print STDERR "\n\n";
 	print STDERR "\t-h | --help: prints help\n";
         exit;
@@ -47,6 +49,7 @@ GetOptions(     "dir=s" => \$dir,
                 "homer" => \$peak,
                 "tag" => \$tag,
 		"bed" => \$bed,
+		"bismark" => \$bismark,
                 "h" => \$help,
                 "help" => \$help)
         or die (&printCMD());
@@ -55,7 +58,7 @@ GetOptions(     "dir=s" => \$dir,
 if($help == 1) {
         &printCMD();
 }
-if($sam == 0 && $peak == 0 && $tag == 0 && $bed == 0) {
+if($sam == 0 && $peak == 0 && $tag == 0 && $bed == 0 && $bismark == 0) {
 	$sam = 1;
 }
 if(@files == 0 && $dir eq "") {
@@ -76,6 +79,10 @@ if(@files == 0) {
 	if($bed == 1) {
 		@files = `ls $dir/*bed`;
 	}
+	if($bismark == 1) {
+		print STDERR "Please specify files!\n";
+		exit;
+	}
 }
 #Check if files and strains have the same length
 if(@strains > 1 && @strains != @files) {
@@ -83,7 +90,7 @@ if(@strains > 1 && @strains != @files) {
 	exit;
 }
 #Remove commas
-my @tmp = split('\s', $files[0]);
+@tmp = split('\s', $files[0]);
 if(@tmp > @files) {
 	@files = @tmp;
 }
@@ -122,6 +129,9 @@ for(my $i = 0; $i < @strains; $i++) {
 	} elsif($bed == 1) {
 		$out_name = substr($files[$i], 0, length($files[$i]) - 4) . "_shifted_from_" . $strains[$i] . ".txt";
 		&shift_bed_file($files[$i], $strains[$i], $out_name);
+	} elsif($bismark == 1) {
+		$out_name = substr($files[$i], 0, length($files[$i]) - 4) . "_shifted_from_" . $strains[$i] . ".txt";
+		&shift_bismark($files[$i], $strains[$i], $out_name);
 	} else {
 		$out_name = $files[$i];
 		while(substr($out_name, length($out_name) - 1) eq "/") {
@@ -162,22 +172,27 @@ sub shift_sam_file{
 	my $file = $_[0];
 	my $strain = $_[1];
 	my $out_file = $_[2];
-	open OUT, ">$out_file" or die "Can't open $out_file: $!\n";
-	open FH, "<$file";
+#	open OUT, ">$out_file" or die "Can't open $out_file: $!\n";
+	open my $out, ">", $out_file or die "Can't open $out_file: $!\n";
+#	open FH, "<$file";
+	open my $fh, "<", $file or die "Can't open $file: $!\n";
 	print STDERR "Reading in " . $file . "\n";
-	foreach my $line (<FH>) {
+	while (my $line = <$fh>) {
+#	foreach my $line (<FH>) {
 		chomp $line;
 		@split = split('\t', $line);
 		@tmp = split("_", $split[2]);
 		#print sam file header
 		if(substr($line, 0, 1) eq "@" && $print_header == 0) {
-			print OUT $line . "\n";
+		#	print OUT $line . "\n";
+			print $out $line . "\n";
 			next;
 		}
 		$print_header++;
 		#print all sequences that could not be mapped
 		if(length($split[2]) < 4) {
-			print OUT $line. "\n";
+		#	print OUT $line. "\n";
+			print $out $line. "\n";
 			next;
 		}
 		$chr = substr($tmp[0], 3);
@@ -188,25 +203,34 @@ sub shift_sam_file{
 		}
 		#chromosomes without mutations can not be shifted		
 		if(!exists $last{$chr}{$allele}) {
-			print OUT $line . "\n";
+		#	print OUT $line . "\n";
+			print $out $line . "\n";
 			next;
 		}
-		print OUT $split[0] . "\t" . $split[1] . "\t" . $split[2] . "\t" . &shift($chr, $allele, $split[3]);
+	#	print OUT $split[0] . "\t" . $split[1] . "\t" . $split[2] . "\t" . &shift($chr, $allele, $split[3]);
+		print $out $split[0] . "\t" . $split[1] . "\t" . $split[2] . "\t" . &shift($chr, $allele, $split[3]);
 		for(my $j = 4; $j < @split; $j++) {
-			print OUT "\t" . $split[$j];
+		#	print OUT "\t" . $split[$j];
+			print $out "\t" . $split[$j];
 		}
-		print OUT "\n";
+	#	print OUT "\n";
+		print $out "\n";
 	}
-	close OUT;
+#	close OUT;
+	close $out;
+	close $fh;
 }
 
 sub shift_peak_file{
 	my $file = $_[0];
 	my $strain = $_[1];
 	my $out_file = $_[2];
-	open OUT, ">$out_file" or die "Can't open $out_file: $!\n";
-	open FH, "<$file";
-	foreach my $line (<FH>) {
+	my $start;
+	my $end;
+
+        open my $out, ">", $out_file or die "Can't open $out_file: $!\n";
+	open my $fh, "<", $file or die "Can't open $file: $!\n";
+	while (my $line = <$fh>) {
 		chomp $line;
 		@split = split('\t', $line);
 		@tmp = split("_", $split[1]);
@@ -218,16 +242,20 @@ sub shift_peak_file{
 		}
 		#chromosomes without mutations can not be shifted
 		if(!exists $last{$chr}{$allele}) {
-			print OUT $line . "\n";
+			print $out $line . "\n";
 			next;
 		}
-		print OUT $split[0] . "\t" . $split[1] . "\t" . &shift($chr, $allele, $split[2]) . "\t" . &shift($chr, $allele, $split[3]);
+		$start = &shift($chr, $allele, $split[2]);
+		$end = &shift($chr, $allele, $split[3]);
+		if($end < $start) { $end = $start + 1; }
+		print $out $split[0] . "\t" . $split[1] . "\t" . $start . "\t" . $end;
 		for(my $j = 4; $j < @split; $j++) {
-			print OUT "\t" . $split[$j];
+			print $out "\t" . $split[$j];
 		}
-		print OUT "\n";
+		print $out "\n";
 	}
-	close OUT;
+	close $out;
+	close $fh;
 }
 
 sub shift_tag_directory{
@@ -248,7 +276,15 @@ sub shift_tag_directory{
 		print STDERR "\n";
 		`rm -rf $output_dir/*`;
 	} else {
-		`mkdir $output_dir`;
+		my $error = "tmp" . rand(15);
+		`mkdir $output_dir 2> $error`;
+		my $lll = `wc -l $error`;
+		if((split('\s+', $lll))[0] > 0) {
+			`cat $error`;
+			`rm $error`;
+			exit;
+		}
+		`rm $error`;
 	}
 	#Copy txt files
 	`cp $directory/*txt $output_dir`;
@@ -256,9 +292,9 @@ sub shift_tag_directory{
 		chomp $file;
 		@split = split("/", $file);
 		$file = $split[-1];
-		open OUT, ">$output_dir/$file";
-		open FH, "<$directory/$file";
-		foreach my $line (<FH>) {
+		open my $out, ">", $output_dir . "/" . $file or die "Can't open " . $output_dir . "/$file: $!\n";
+		open my $fh, "<", $directory . "/" . $file or die "Can't open " . $directory . "/$file: $!\n";
+		while (my $line = <$fh>) {
 			chomp $line;
 			@split = split('\t', $line);
 			@tmp = split("_", $split[1]);
@@ -268,10 +304,10 @@ sub shift_tag_directory{
 			} else {
 				$allele = 1;
 			}
-			print OUT $split[0] . "\t" . $split[1] . "\t" . &shift($chr, $allele, $split[2]) . "\t" . $split[3] . "\t" . $split[4] . "\t" . $split[5] . "\n";
+			print $out $split[0] . "\t" . $split[1] . "\t" . &shift($chr, $allele, $split[2]) . "\t" . $split[3] . "\t" . $split[4] . "\t" . $split[5] . "\n";
 		}
-		close OUT;
-		close FH;
+		close $out;
+		close $fh;
 	}
 }
 
@@ -280,9 +316,14 @@ sub shift_bed_file{
 	my $strain = $_[1];
 	my $out_file = $_[2];
 	print STDERR "shifting " . $file . "\n";
-	open OUT, ">$out_file" or die "Can't open file!\n";
-	open FH, "<$file";
-	foreach my $line (<FH>) {
+
+
+	open my $out, ">", $out_file or die "Can't open $out_file: $!\n";
+	open my $fh, "<", $file or die "Can't open $file: $!\n";
+	my $start;
+	my $end;
+	my $allele = 1;
+	while(my $line = <$fh>) {
 		chomp $line;
 		@split = split('\t', $line);
 		@tmp = split("_", $split[0]);
@@ -293,11 +334,49 @@ sub shift_bed_file{
 			$allele = 1;
 		}
 		if(!exists $last{$chr}{$allele}) {
-			print OUT $line . "\n";
+			print $out $line . "\n";
 		} else {
-			print OUT $tmp[0] . "\t" . &shift($chr, $allele, $split[1]) . "\t" . &shift($chr, $allele, $split[2]) . "\n";
+			$start = &shift($chr, $allele, $split[1]);
+			$end = &shift($chr, $allele, $split[2]);
+			if($end < $start) { $end = $start + 1; }
+			print $out $tmp[0] . "\t" . $start . "\t" . $end . "\t" . $split[3] . "\n";
 		}
 	}
-	close FH;
-	close OUT;
+	close $fh;
+	close $out;
+}
+
+sub shift_bismark{
+	my $file = $_[0];
+	my $strain = $_[1];
+	my $out_file = $_[2];
+	print STDERR "shifting " . $file . "\n";
+	open my $out, ">", $out_file or die "Can't open $out_file: $!\n";
+	open my $fh, "<", $file or die "Can't open $file: $!\n";
+	my $start;
+	my $end;
+	my $allele = 1;
+	while(my $line = <$fh>) {
+		chomp $line;
+		@split = split('\t', $line);
+		@tmp = split("_", $split[0]);
+		$chr = substr($tmp[0], 3);
+		if(@tmp > 1) {
+			$allele = $tmp[2];
+		} else {
+			$allele = 1;
+		}
+		if(!exists $last{$chr}{$allele}) {
+			print $out $line . "\n";
+		} else {
+			$start = &shift($chr, $allele, $split[1]);
+			print $out $tmp[0] . "\t" . $start;
+			for(my $i = 2; $i < @split; $i++) {
+				print $out "\t" . $split[$i];
+			}
+			print $out "\n";
+		}
+	}
+	close $fh;
+	close $out;
 }
